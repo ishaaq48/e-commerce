c07e1df073d202a64776c4863126f2fc4c13aba7
Syed Ishaaq <syedishaaq48@gmail.com>
2024-12-20 12:31:47 +0530

    rating-productscreen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,15 +6,16 @@ import {
   } from 'react-router-dom'
 import './assets/bootstrap.custom.css'
 import './assets/index.css'
-
 import App from './App.jsx'
 import HomeScreen from './Screen/HomeScreen.jsx'
+import ProductScreen from './Screen/ProductScreen.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Routes>
       <Route path='/' element={<App />}>
         <Route index path='/' element={<HomeScreen />} />
+        <Route path='/product/:id' element={<ProductScreen />} />
       </Route>
   </Routes>
   </BrowserRouter>
