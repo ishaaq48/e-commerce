0e22d9740b9dad2d6c42f1841d8a1b26cc865c01
Syed Ishaaq <syedishaaq48@gmail.com>
2024-12-20 11:12:48 +0530

    Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Container } from "react-bootstrap"
+import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
-import HomeScreen from "./Screen/HomeScreen"
 import Footer from "./components/Footer"
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-           <HomeScreen />  
+            <Outlet /> 
         </Container> 
       </main>
       <Footer />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,21 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { 
+  Route,
+  BrowserRouter,
+  Routes
+  } from 'react-router-dom'
 import './assets/bootstrap.custom.css'
 import './assets/index.css'
+
 import App from './App.jsx'
+import HomeScreen from './Screen/HomeScreen.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+  <Routes>
+      <Route path='/' element={<App />}>
+        <Route index path='/' element={<HomeScreen />} />
+      </Route>
+  </Routes>
+  </BrowserRouter>
 )
