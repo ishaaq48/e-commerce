862d31f8e399da8a566d2c08f0333f7169d7eb43
Syed Ishaaq <syedishaaq48@gmail.com>
2024-12-24 18:41:20 +0530

    cart-functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,10 @@
-import { Navbar,Nav,Container } from 'react-bootstrap'
+import { Navbar,Nav,Container,Badge } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import {Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 const Header = () => {
+    const { cartItems } = useSelector(state => state.cart)
+    
   return (
     <header>
         <Navbar bg="dark" variant = "dark" expand="md" collapseOnSelect>
@@ -14,6 +17,11 @@ const Header = () => {
                     <Nav className='ms-auto'>
                         <Link to="/cart" className="nav-link">
                             <FaShoppingCart /> Cart
+                            {cartItems.length > 0 && (
+                                <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                                    {cartItems.reduce((a, c) => a + c.qty, 0)}
+                                </Badge>
+                            )}
                         </Link>
                         <Link to="/login" className="nav-link">
                             <FaUser />Sign In
