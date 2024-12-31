b6b05422f087374fc93c8013f4989d06a17de5f6
Syed Ishaaq <syedishaaq48@gmail.com>
2024-12-31 11:43:51 +0530

    server-update

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,33 @@
-import path from 'path'
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-dotenv.config()
-import connectDB from './config/db.js'
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
-import { notFound,errorHandler } from './middleware/errorMiddleware.js'
-import uploadRoutes from './routes/uploadRoutes.js'
-connectDB()
-const app = express()
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const port = process.env.PORT || 5000;
 
-app.use(cookieParser())
-const PORT = process.env.PORT || 5000
+connectDB();
 
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
-app.use('/api/upload',uploadRoutes)
+const app = express();
 
-app.get('/api/config/paypal',(req,res) => 
-    res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
-)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
@@ -46,10 +45,9 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
